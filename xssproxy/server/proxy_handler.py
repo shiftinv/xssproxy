31ab06af01a05e7a48d9bdaadaf3f794a85a1197
shiftinv <me@shiftinv.cc>
2021-02-22 17:42:37 +0100

    Handle invalid proxy requests, use custom status codes

--- xssproxy/server/proxy_handler.py
+++ xssproxy/server/proxy_handler.py
@@ -8,7 +8,10 @@ async def handler(request: web.BaseRequest):
 
     websocket = getattr(request.protocol._manager, 'websocket_storage').get()
     if websocket is None:
-        return web.Response(status=418, text='no websocket connected')
+        return web.Response(status=553, text='no websocket connected')
+
+    if not request.raw_path.startswith('http://'):
+        return web.Response(status=554, text='invalid url')
 
     remote_response = await WebsocketRemote.http_request(
         websocket,
@@ -22,7 +25,7 @@ async def handler(request: web.BaseRequest):
         err = remote_response['error']
         logger.error(f'received error: {err}')
         return web.Response(
-            status=418,
+            status=555,
             text=f'request failed (error: {err})'
         )
     else:
