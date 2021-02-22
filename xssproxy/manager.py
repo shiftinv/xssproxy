935c8f634f49f29bc7eff7bb308c0ecc3722e784
shiftinv <me@shiftinv.cc>
2021-02-22 16:52:35 +0100

    Fix websocket disconnect on context exit

--- xssproxy/manager.py
+++ xssproxy/manager.py
@@ -13,12 +13,18 @@ class ServerManager:
 
     def __init__(self):
         self.runners = []
+        self.web_app = None
+        self.proxy_server = None
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
         loop = asyncio.get_event_loop()
+        if self.web_app:
+            ws = self.web_app['websocket_storage'].get()
+            if ws is not None:
+                loop.run_until_complete(ws.close())
         loop.run_until_complete(asyncio.gather(*[runner.cleanup() for runner in self.runners]))
 
     def __get_logging_params(self, type_name) -> Dict[str, logging.Logger]:
@@ -41,27 +47,27 @@ class ServerManager:
         #     -> as a workaround, pass to web.Server directly
 
         # initialize javascript/websocket server
-        web_app = web.Application(logger=self.__get_logging_params('webserver')['logger'])
-        web_handler.setup_app(web_app)
+        self.web_app = web.Application(logger=self.__get_logging_params('webserver')['logger'])
+        web_handler.setup_app(self.web_app)
 
         # initialize local proxy server
-        proxy_server = web.Server(
+        self.proxy_server = web.Server(
             None,
             **self.__get_logging_params('proxyserver')
         )
-        proxy_handler.setup_server(proxy_server)
+        proxy_handler.setup_server(self.proxy_server)
 
         # create websocket storage for sharing websockets between web server and proxy server
         # TODO: this is kinda messy
         storage = WebsocketStorage()
-        web_app['websocket_storage'] = storage
-        setattr(proxy_server, 'websocket_storage', storage)
+        self.web_app['websocket_storage'] = storage
+        setattr(self.proxy_server, 'websocket_storage', storage)
 
         # start both servers
         self._logger.info(f'starting web server')
         await self.start_runner(
             web.AppRunner(
-                web_app,
+                self.web_app,
                 **self.__get_logging_params('webserver')
             ),
             args.web_host,
@@ -71,7 +77,7 @@ class ServerManager:
 
         self._logger.info(f'starting proxy server')
         await self.start_runner(
-            web.ServerRunner(proxy_server),
+            web.ServerRunner(self.proxy_server),
             args.proxy_host,
             args.proxy_port
         )
